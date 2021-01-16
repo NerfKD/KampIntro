f9a6f702e793d792e533bf1dba835a35aa64ab67
Kerim Dinçer <kerim_dincer@hotmail.com>
2021-01-16 23:49:14 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Kerim";
+            int yas = 29;
+
+            Kurs kurs1 = new Kurs();
+
+            kurs1.KursAdi = "C#";
+            kurs1.Egitmen = "Kerim Dinçer";
+            kurs1.IzlenmeOrani = 10;
+
+            Kurs kurs2 = new Kurs();
+
+            kurs2.KursAdi = "Java";
+            kurs2.Egitmen = "Emre Dinçer";
+            kurs2.IzlenmeOrani = 15;
+
+            Kurs kurs3 = new Kurs();
+
+            kurs3.KursAdi = "Python";
+            kurs3.Egitmen = "Melih Esmer";
+            kurs3.IzlenmeOrani = 13;
+
+            //Console.WriteLine(kurs1.KursAdi + " : " + kurs1.Egitmen);
+            //Console.WriteLine(kurs2.KursAdi + " : " + kurs2.Egitmen);
+            //Console.WriteLine(kurs3.KursAdi + " : " + kurs3.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.KursAdi + " : " + kurs.Egitmen);
+            }
+        }
+    }
+
+
+    class Kurs
+    {
+        public string KursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
---
+++ DegerVeReferansTipler/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DegerVeReferansTipler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int, decimal, float, double, bool = değer tip
+            //array,class,interface = referans tip
+            //stack(değer tipler tutulur) ve heap(referans tipler içerikleri tutulur, aliasları gene stackde tutulur) olarak bellekde iki yerimiz mevcut.
+
+            int sayi1 = 10;
+            int sayi2 = 30;
+            sayi1 = sayi2;
+            sayi2 = 65;
+
+            Console.WriteLine(sayi1); //30 olur cevap
+            //sadece stackte tutulduğu için adres mantığı olmadığı için problem olmaz.
+
+            int[] sayilar1 = new int[] { 10, 20, 30 };
+            int[] sayilar2 = new int[] { 100, 200, 300 };
+            sayilar1 = sayilar2;
+            sayilar2[0] = 999;
+            Console.WriteLine(sayilar1[0]); //999 olur cevap
+            //diziler eşitlendiğinde stack de bulunan dizi alias ının sadece heap de ki adresi değiştiri. sayilar1 = sayilar2 olduğunda sayilar1 dizisi sayilar2 nin heap de ki adresini alır.
+            //Bu sebeple sayilar2[0] değiştiğinde sayilar1[0] ekrana yazdırılmak istense adresleri ortak olduğu için 999 gelecektir.
+        }
+    }
+}
